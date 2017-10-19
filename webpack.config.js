69634cfc9dbe3ccd7bbb83a25794e7efc6a02ca3
wangbiaoxuan <wangbiaoxuan-rj@ofo.com>
2017-10-19 16:46:12 +0800

    feat: show what's wrong if i don't set chunkFileName

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,20 @@
-const path = require('path');const HTMLWebpackPlugin = require('html-webpack-plugin');module.exports = {    entry: {        index: './src/index.js'    },    plugins: [        new HTMLWebpackPlugin({            title: 'Code Splitting'        })    ],    output: {        filename: '[name].bundle.js',        chunkFilename: '[name].bundle.js',        path: path.resolve(__dirname, 'dist')    },    devServer: {        contentBase: './dist'    }};
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: {
+        index: './src/index.js'
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: 'Code Splitting'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        // chunkFilename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+};
